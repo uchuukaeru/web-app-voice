14dec7a6a30df47d38c4b31b92fed7c762364da4
kaeru <83284713+uchuukaeru@users.noreply.github.com>
2025-08-27 22:43:34 +0900

    Update server.js
    serverからDeno.serveに変更した

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.180.0/http/file_server.ts";
 
-serve(async (req) => {
+Deno.serve({ port: 8080 }, async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
 
